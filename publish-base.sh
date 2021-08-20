297efbc16c4367263885d084a785e40d3ea38107
shoothzj <shoothzj@gmail.com>
2021-08-20 20:12:44 +0800

    first commit

---
+++ publish-base.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+
+bash $DIR/build-push-base.sh latest
+
+bash $DIR/build-push-base.sh centos
+bash $DIR/build-push-base.sh ubuntu
+
+bash $DIR/build-push-base.sh erlang
+bash $DIR/build-push-base.sh go
+bash $DIR/build-push-base.sh go114
+bash $DIR/build-push-base.sh goc
+bash $DIR/build-push-base.sh jdk8
+bash $DIR/build-push-base.sh jdk11
+bash $DIR/build-push-base.sh jdk11-ssh
+bash $DIR/build-push-base.sh node
+bash $DIR/build-push-base.sh network
+
+bash $DIR/build-push-base.sh clojure
